bb671a51838d7e218474085e75690dd199e7e052
fernandobosco <fbosco@dlocal.com>
2022-01-19 16:36:26 -0300

    Initial Commit

---
+++ FastingTimer/FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Fernando Bosco on 19/01/22.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
